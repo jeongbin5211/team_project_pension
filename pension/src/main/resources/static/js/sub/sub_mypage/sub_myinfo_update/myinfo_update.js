bf7e4d06d37659447d19d1245421b990e1db95ee
Hwangseongyoung <dfghukl1333@naver.com>
2023-11-29 06:41:04 +0900

    reservation

--- pension/src/main/resources/static/js/sub/sub_mypage/sub_myinfo_update/myinfo_update.js
+++ pension/src/main/resources/static/js/sub/sub_mypage/sub_myinfo_update/myinfo_update.js
@@ -1,53 +1,89 @@
 let userid_change_btn = document.querySelector(".userid_change_btn");
-        let userid_change = document.querySelector(".userid_change");
+let userid_change = document.querySelector(".userid_change");
 
-        userid_change_btn.addEventListener('click', function(e) {
-            e.preventDefault();
+userid_change_btn.addEventListener('click', function(e) {
+    e.preventDefault();
 
-            userid_change.classList.toggle("userid_box");
-            if(userid_change.classList.contains("userid_box")){
-                userid_change_btn.innerText = "아이디(이메일) 변경 취소";
-                userid_change_btn.style.backgroundColor = "#999";
-                userid_change_btn.style.color = "#fff";
-            }else {
-                userid_change_btn.innerText = "아이디(이메일) 변경";
-                userid_change_btn.style.backgroundColor = "#eee";
-                userid_change_btn.style.color = "#555";
-            }
-        });
+    userid_change.classList.toggle("userid_box");
+    if(userid_change.classList.contains("userid_box")){
+        userid_change_btn.innerText = "아이디 변경 취소";
+        userid_change_btn.style.backgroundColor = "#999";
+        userid_change_btn.style.color = "#fff";
+    }else {
+        userid_change_btn.innerText = "아이디 변경";
+        userid_change_btn.style.backgroundColor = "#eee";
+        userid_change_btn.style.color = "#555";
+    }
+});
 
-        let username_change_btn = document.querySelector(".username_change_btn");
-        let username_change = document.querySelector(".username_change");
+let username_change_btn = document.querySelector(".username_change_btn");
+let username_change = document.querySelector(".username_change");
 
-        username_change_btn.addEventListener('click', function(e) {
-            e.preventDefault();
+username_change_btn.addEventListener('click', function(e) {
+    e.preventDefault();
 
-            username_change.classList.toggle("username_box");
-            if(username_change.classList.contains("username_box")){
-                username_change_btn.innerText = "이름변경 취소";
-                username_change_btn.style.backgroundColor = "#999";
-                username_change_btn.style.color = "#fff";
-            }else {
-                username_change_btn.innerText = "이름변경";
-                username_change_btn.style.backgroundColor = "#eee";
-                username_change_btn.style.color = "#555";
-            }
-        });
+    username_change.classList.toggle("username_box");
+    if(username_change.classList.contains("username_box")){
+        username_change_btn.innerText = "이름변경 취소";
+        username_change_btn.style.backgroundColor = "#999";
+        username_change_btn.style.color = "#fff";
+    }else {
+        username_change_btn.innerText = "이름변경";
+        username_change_btn.style.backgroundColor = "#eee";
+        username_change_btn.style.color = "#555";
+    }
+});
 
-        let usertell_change_btn = document.querySelector(".usertell_change_btn");
-        let usertell_change = document.querySelector(".usertell_change");
+let usertell_change_btn = document.querySelector(".usertell_change_btn");
+let usertell_change = document.querySelector(".usertell_change");
 
-        usertell_change_btn.addEventListener('click', function(e) {
-            e.preventDefault();
+usertell_change_btn.addEventListener('click', function(e) {
+    e.preventDefault();
 
-            usertell_change.classList.toggle("usertell_box");
-            if(usertell_change.classList.contains("usertell_box")){
-                usertell_change_btn.innerText = "휴대폰 번호 변경 취소";
-                usertell_change_btn.style.backgroundColor = "#999";
-                usertell_change_btn.style.color = "#fff";
-            }else {
-                usertell_change_btn.innerText = "휴대폰 번호 변경";
-                usertell_change_btn.style.backgroundColor = "#eee";
-                usertell_change_btn.style.color = "#555";
-            }
-        });
+    usertell_change.classList.toggle("usertell_box");
+    if(usertell_change.classList.contains("usertell_box")){
+        usertell_change_btn.innerText = "전화번호 변경 취소";
+        usertell_change_btn.style.backgroundColor = "#999";
+        usertell_change_btn.style.color = "#fff";
+    }else {
+        usertell_change_btn.innerText = "전화번호 변경";
+        usertell_change_btn.style.backgroundColor = "#eee";
+        usertell_change_btn.style.color = "#555";
+    }
+});
+
+let useremail_change_btn = document.querySelector(".useremail_change_btn");
+let useremail_change = document.querySelector(".useremail_change");
+
+useremail_change_btn.addEventListener('click', function(e) {
+    e.preventDefault();
+
+    useremail_change.classList.toggle("useremail_box");
+    if(useremail_change.classList.contains("useremail_box")){
+        useremail_change_btn.innerText = "이메일 변경 취소";
+        useremail_change_btn.style.backgroundColor = "#999";
+        useremail_change_btn.style.color = "#fff";
+    }else {
+        useremail_change_btn.innerText = "이메일 번호 변경";
+        useremail_change_btn.style.backgroundColor = "#eee";
+        useremail_change_btn.style.color = "#555";
+    }
+});
+
+let useraddr_change_btn = document.querySelector(".useraddr_change_btn");
+let useraddr_change = document.querySelector(".useraddr_change");
+
+useraddr_change_btn.addEventListener('click', function(e) {
+    e.preventDefault();
+
+    useraddr_change.classList.toggle("useraddr_box");
+    if(useraddr_change.classList.contains("useraddr_box")){
+        useraddr_change_btn.innerText = "이메일 변경 취소";
+        useraddr_change_btn.style.backgroundColor = "#999";
+        useraddr_change_btn.style.color = "#fff";
+    }else {
+        useraddr_change_btn.innerText = "이메일 번호 변경";
+        useraddr_change_btn.style.backgroundColor = "#eee";
+        useraddr_change_btn.style.color = "#555";
+    }
+});
---
+++ pension/src/main/resources/static/js/sub/sub_reserve/sub_check_room/calendar.js
@@ -0,0 +1,186 @@
+// 윤년 계산 함수
+    function checkLeapYear() {
+      if(year%400 == 0) {
+        return true;
+      }else if(year%100 == 0) {
+        return false;
+      }else if(year%4 == 0) {
+        return true;
+      }else {
+        return false;
+      }
+    }
+
+    // 1일이 무슨 요일인지 구하는 함수
+    function getFirstDatOfWeek(year, month) {
+      if(month < 10) {
+        month = "0" + month;
+      }
+      return (new Date(year + "-" + month + "-01" )).getDay();
+    }
+
+    // 년도, 월을 직접 변경하였을 때 바뀌는 함수
+    function changeYearMonth(year, month) {
+      let month_day = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+      if(month == 2) {
+        if(checkLeapYear(year)) {
+          month_day[1] = 29;
+        }
+      }
+      let first_day_of_week = getFirstDatOfWeek(year, month);
+      
+      let arr_calender = [];
+      for(let i=0;i<first_day_of_week;i++) {
+        arr_calender.push("");
+      }
+      for(let i=1;i<=month_day[month-1];i++) {
+        arr_calender.push(String(i));
+      }
+
+      // 아래쪽 빈 공간에 채워질 배열 생성
+      let remain_day = 7 - (arr_calender.length%7);
+      if(remain_day < 7) {
+        for(let i=0;i<remain_day;i++) {
+          arr_calender.push("");
+        }
+      }
+      renderCalender(arr_calender);
+    }
+    
+    // 1~31일까지 테이블 html생성
+    function renderCalender(data) {
+      let h = [];
+      for(let i=0;i<data.length;i++) {
+        if(i == 0) {
+          h.push('<tr>');
+        }else if(i%7 == 0) {
+          h.push('</tr>');
+          h.push('<tr>');
+        }
+
+        if(current_year < now_year) {
+          h.push('<td style="cursor: default; opacity: 0.5;">'+data[i]+'</td>');
+        }else if(current_year == now_year){
+          if(current_month < now_month) {
+            h.push('<td style="cursor: default; opacity: 0.5;">'+data[i]+'</td>');
+          }else if(current_month == now_month) {
+            if(data[i]>=current_date) {
+              if(data[i] == ""){
+                h.push('<td>'+data[i]+'</td>');
+              }else {
+                h.push('<td onclick="setDate(' + data[i] + ');" style="cursor: pointer;">'+data[i]+'</td>');
+              }
+            }else {
+              h.push('<td style="cursor: default; opacity: 0.5;">'+data[i]+'</td>');
+            }
+          }else {
+            if(data[i] == "") {
+              h.push('<td>'+data[i]+'</td>');
+            }else {
+              h.push('<td onclick="setDate(' + data[i] + ');" style="cursor: pointer;">'+data[i]+'</td>');
+            }
+          }
+        }else if(current_year > now_year) {
+          if(data[i] == "") {
+            h.push('<td>'+data[i]+'</td>');
+          }else {
+            h.push('<td onclick="setDate(' + data[i] + ');" style="cursor: pointer;">'+data[i]+'</td>');
+          }
+        }
+      }
+      h.push('</tr>');
+      $("#tb_body").html(h.join(""));
+    }
+
+    // 날짜를 클릭 했을때 input값에 넣기 함수
+    let checkin = document.querySelector("#input_date_checkin");
+    let checkout = document.querySelector("#input_date_checkout");
+    
+    console.log(checkin);
+    function setDate(day) {
+      if(day < 10) {
+        day = "0" + day;
+      }
+      if(checkin.value == "") {
+        if(current_month < 10){
+          $("#input_date_checkin").val(current_year+"-0"+current_month+"-"+day);
+        }else {
+          $("#input_date_checkin").val(current_year+"-"+current_month+"-"+day);
+        }
+      }else {
+        if(current_month < 10){
+          $("#input_date_checkout").val(current_year+"-0"+current_month+"-"+day);
+        }else {
+          $("#input_date_checkout").val(current_year+"-"+current_month+"-"+day);
+        }      
+      }
+      if(checkin.value && checkout.value != ""){
+        if(checkin.value >= checkout.value) {
+          alert("예약날짜를 다시 확인해 주세요.");
+          checkin.value = "";
+          checkout.value = "";
+        }
+      }
+    }
+    
+    // 날짜 입력값 초기화
+    let reset_btn = document.querySelector(".reset_checkdate");
+
+    reset_btn.addEventListener('click', function(e) {
+      e.preventDefault();
+      
+      checkin.value = "";
+      checkout.value = "";
+    });
+    
+    // 현재 년, 월, 일저장
+    let current_year = (new Date()).getFullYear();
+    let current_month = (new Date()).getMonth() + 1;
+
+    // 현재 날짜(년, 월, 일)
+    let now_year = (new Date()).getFullYear();
+    let now_month = (new Date()).getMonth()+1;
+    let current_date = (new Date()).getDate();
+
+    $("#year").val(current_year);
+    $("#month").val(current_month);
+
+    // 년도 수정하면 바뀌는 함수
+    function changeYear() {
+      current_year = parseInt($("#year").val());
+      loadCalender();
+    }
+
+    // 버튼으로 월 이동하는 함수
+    function changeMonth(diff) {
+      if(diff == undefined) {
+        current_month = parseInt($("#month").val());
+      }else{
+        current_month = current_month + diff;
+        if(current_month == 0) {
+          current_year = current_year - 1;
+          current_month = 12;
+        }else if(current_month == 13) {
+          current_year = current_year + 1;
+          current_month = 1;
+        }
+      }
+      loadCalender();
+    }
+    
+    function loadCalender() {
+      $("#year").val(current_year);
+      $("#month").val(current_month);
+      changeYearMonth(current_year, current_month);
+    }
+
+    changeYearMonth(current_year, current_month);
+
+let cld = document.querySelector(".cld_wrap");
+let cld_btn = document.querySelector(".cld_show");
+
+cld_btn.addEventListener('click', function(e) {
+  e.preventDefault();
+  cld.classList.toggle("cld_toggle");
+});
