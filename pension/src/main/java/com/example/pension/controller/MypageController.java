d1d2c0637a437123f2358a6120bca386254f173f
Hwangseongyoung <dfghukl1333@naver.com>
2023-12-19 18:35:21 +0900

    회원정보 수정 및 탈퇴

--- pension/src/main/java/com/example/pension/controller/MypageController.java
+++ pension/src/main/java/com/example/pension/controller/MypageController.java
@@ -2,6 +2,7 @@ package com.example.pension.controller;
 
 import com.example.pension.dto.MemberDto;
 import com.example.pension.dto.ReserveListDto;
+import com.example.pension.mappers.MypageMapper;
 import com.example.pension.service.MypageService;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,20 +22,137 @@ public class MypageController {
     @Autowired
     MypageService mypageService;
 
+    @Autowired
+    MypageMapper mypageMapper;
+
     @GetMapping("/myinfo/checkpw")
-    public String getCheckpw() {
+    public String getCheckpw(HttpSession hs, Model model) {
+        MemberDto memberDto = (MemberDto) hs.getAttribute("user");
+        model.addAttribute("member", memberDto);
+
         return "sub_pages/sub_mypage/sub_myinfo_checkpw/myinfo_checkpw.html";
     }
 
-    @GetMapping("/myinfo/sece")
-    public String getSece() {
-        return "sub_pages/sub_mypage/sub_myinfo_secession/myinfo_secession.html";
+    @PostMapping("/myinfo/checkpw")
+    @ResponseBody
+    public Map<String, Object> setCheckpw(@RequestParam int id, @RequestParam String userpw) {
+        Map<String, Object> map = new HashMap<>();
+        String alert = mypageService.checkpw(id, userpw);
+        if(alert.equals("success")) {
+            map.put("msg", "success");
+        }else if(alert.equals("failure")) {
+            map.put("msg", "failure");
+        }
+        return map;
     }
 
     @GetMapping("/myinfo/update")
-    public String getUpdate() {
+    public String getUpdate(HttpSession hs, Model model) {
+        MemberDto memberDto = (MemberDto) hs.getAttribute("user");
+        model.addAttribute("member", memberDto);
         return "sub_pages/sub_mypage/sub_myinfo_update/myinfo_update.html";
     }
+
+    @PostMapping("/changeUserid")
+    @ResponseBody
+    public Map<String, Object> setUpdateUserid(@RequestParam int id, @RequestParam String userid, HttpSession hs) {
+        Map<String, Object> map = new HashMap<>();
+        String alert = mypageService.checkUserid(userid);
+        if(alert.equals("success")) {
+            mypageMapper.setUpdateUserid(userid, id);
+            hs.invalidate();
+            map.put("msg", "success");
+        }else if(alert.equals("failure")) {
+            map.put("msg", "failure");
+        }
+        return map;
+    }
+
+    @PostMapping("/changeUserpw")
+    @ResponseBody
+    public Map<String, Object> setUpdateUserpw(@RequestParam int id, @RequestParam String userpw, @RequestParam String newUserpw, HttpSession hs) {
+        Map<String, Object> map = new HashMap<>();
+        String alert = mypageService.checkpw(id, userpw);
+        if(alert.equals("success")) {
+            mypageMapper.setUpdateUserpw(newUserpw, id);
+            hs.invalidate();
+            map.put("msg", "success");
+        }else {
+            map.put("msg", "failure");
+        }
+        return map;
+    }
+
+    @PostMapping("/changeName")
+    @ResponseBody
+    public Map<String, Object> setUpdateName(@RequestParam int id, @RequestParam String name, HttpSession hs) {
+        Map<String, Object> map = new HashMap<>();
+        mypageMapper.setUpdateName(name, id);
+        MemberDto memberDto = (MemberDto) hs.getAttribute("user");
+        memberDto.setName(name);
+        hs.setAttribute("user", memberDto);
+        map.put("msg", "success");
+        return map;
+    }
+
+    @PostMapping("/changePhone")
+    @ResponseBody
+    public Map<String, Object> setUpdatePhone(@RequestParam int id, @RequestParam String phone, HttpSession hs) {
+        Map<String, Object> map = new HashMap<>();
+        mypageMapper.setUpdatePhone(phone, id);
+        MemberDto memberDto = (MemberDto) hs.getAttribute("user");
+        memberDto.setPhone(phone);
+        hs.setAttribute("user", memberDto);
+        map.put("msg", "success");
+        return map;
+    }
+
+    @PostMapping("/changeEmail")
+    @ResponseBody
+    public Map<String, Object> setUpdateEmail(@RequestParam int id, @RequestParam String email, HttpSession hs) {
+        Map<String, Object> map = new HashMap<>();
+        mypageMapper.setUpdateEmail(email, id);
+        MemberDto memberDto = (MemberDto) hs.getAttribute("user");
+        memberDto.setEmail(email);
+        hs.setAttribute("user", memberDto);
+        map.put("msg", "success");
+        return map;
+    }
+
+    @PostMapping("/changeAddr")
+    @ResponseBody
+    public Map<String, Object> setUpdateAddr(@RequestParam int id, @RequestParam String addr, HttpSession hs) {
+        Map<String, Object> map = new HashMap<>();
+        mypageMapper.setUpdateAddr(addr, id);
+        MemberDto memberDto = (MemberDto) hs.getAttribute("user");
+        memberDto.setAddr(addr);
+        hs.setAttribute("user", memberDto);
+        map.put("msg", "success");
+        return map;
+    }
+
+    @GetMapping("/myinfo/sece")
+    public String getSece(HttpSession hs, Model model) {
+        MemberDto memberDto = (MemberDto) hs.getAttribute("user");
+        model.addAttribute("member", memberDto);
+        return "sub_pages/sub_mypage/sub_myinfo_secession/myinfo_secession.html";
+    }
+
+    @PostMapping("/secessionMember")
+    @ResponseBody
+    public Map<String, Object> setSeceMember(@RequestParam int id, HttpSession hs) {
+        Map<String, Object> map = new HashMap<>();
+        String alert = mypageService.checkReserve(id);
+        if(alert.equals("success")) {
+            mypageMapper.setSeceMember(id);
+            hs.invalidate();
+            map.put("msg", "success");
+        }else if(alert.equals("failure")) {
+            map.put("msg", "failure");
+        }
+        return map;
+    }
+
 
     @GetMapping("/reserveList")
     public String getReserveList(HttpSession hs, Model model) {
--- pension/src/main/resources/static/js/sub/sub_mypage/sub_myinfo_update/myinfo_update_exception.js
+++ pension/src/main/resources/static/js/sub/sub_mypage/sub_myinfo_update/myinfo_update_exception.js
@@ -1,94 +1,194 @@
-let btn_change_userid = document.querySelector("#btn_change_userid");
+let id = document.querySelector("input[name=id]");
 let userid = document.querySelector(".userid");
-let btn_change_username = document.querySelector("#btn_change_username");
-let username = document.querySelector(".username");
-let btn_change_usertell = document.querySelector("#btn_change_usertell");
-let usertell = document.querySelector(".usertell");
-let btn_change_useremail = document.querySelector("#btn_change_useremail");
-let useremail = document.querySelector(".useremail");
-let btn_change_useraddr = document.querySelector("#btn_change_useraddr");
-let useraddr = document.querySelector(".useraddr");
-
-btn_change_userid.addEventListener('click', function(e) {
-    e.preventDefault();
-
-    if(userid.value == "") {
-        alert("변경하실 아이디를 입력해 주세요.");
-        userid.focus();
-        return false;
-    }
-});
-
-btn_change_username.addEventListener('click', function(e) {
-    e.preventDefault();
-
-    if(username.value == "") {
-        alert("변경하실 이름을 입력해 주세요.");
-        username.focus();
-        return false;
-    }
-});
-
-btn_change_usertell.addEventListener('click', function(e) {
-    e.preventDefault();
-
-    if(usertell.value == "") {
-        alert("변경하실 전화번호를 입력해 주세요.");
-        usertell.focus();
-        return false;
-    }
-});
-
-btn_change_useremail.addEventListener('click', function(e) {
-    e.preventDefault();
-
-    if(useremail.value == "") {
-        alert("변경하실 이메일을 입력해 주세요.");
-        useremail.focus();
-        return false;
-    }
-});
-
-btn_change_useraddr.addEventListener('click', function(e) {
-    e.preventDefault();
-
-    if(useraddr.value == "") {
-        alert("변경하실 주소를 입력해 주세요.");
-        useraddr.focus();
-        return false;
-    }
-});
-
-let userpasswd = document.querySelector("#userpasswd");
+let name = document.querySelector(".username");
+let phone = document.querySelector(".usertell");
+let email = document.querySelector(".useremail");
+let addr = document.querySelector(".useraddr");
+let userpw = document.querySelector("#userpasswd");
 let new_userpasswd = document.querySelector("#new_userpasswd");
 let renew_userpasswd = document.querySelector("#renew_userpasswd");
 
-let passwd_change_btn = document.querySelector("#passwd_change_btn");
+function changeUserid() {
+    if(userid.value == "") {
+        alert("변경하실 아이디를 입력하세요.");
+        userid.focus();
+        return false;
+    }
 
-passwd_change_btn.addEventListener('click', function(e) {
-    e.preventDefault();
+    $.ajax({
+        type: "post",
+        url: "/mypage/changeUserid",
+        dataType: "json",
+        data:{
+            id: id.value,
+            userid: userid.value
+        },
+        success: function(result) {
+            if(result.msg == "success") {
+                alert("아이디가 변경되었습니다.\n로그인을 다시 하여 확인하세요.");
+                location.reload();
+            }else if(result.msg == "failure") {
+                alert("이미 사용중인 아이디입니다.\n다른 아이디로 입력하세요.");
+                userid.value = "";
+                location.reload();
+            }
+        }
+    });
+}
 
+function changeUserpw() {
     if(userpasswd.value == "") {
-        alert("현재 비밀번호를 입력해 주세요.");
+        alert("현재 비밀번호를 입력하세요.");
         userpasswd.focus();
         return false;
     }
     if(new_userpasswd.value == "") {
-        alert("새 비밀번호를 입력해 주세요.");
+        alert("새 비밀번호를 입력하세요.");
         new_userpasswd.focus();
         return false;
     }
     if(renew_userpasswd.value == "") {
-        alert("새 비밀번호를 다시 입력해 주세요.");
+        alert("새 비밀번호를 다시 입력하세요.");
         renew_userpasswd.focus();
         return false;
     }
 
     if(new_userpasswd.value != renew_userpasswd.value) {
-        alert("입력하신 새 비밀번호가 일치하지 않습니다.\n다시 입력해 주세요.");
+        alert("입력하신 새 비밀번호가 일치하지 않습니다.\n다시 입력하세요.");
         new_userpasswd.value = "";
         renew_userpasswd.value = "";
         new_userpasswd.focus();
         return false;
     }
-});
+
+    $.ajax({
+        type: "post",
+        url: "/mypage/changeUserpw",
+        dataType: "json",
+        data:{
+            id: id.value,
+            userpw: userpw.value,
+            newUserpw: new_userpasswd.value
+        },
+        success: function(result) {
+            if(result.msg == "success") {
+                alert("비밀번호가 변경되었습니다.\n로그인을 다시 하여 확인하세요.");
+                location.reload();
+            }else if(result.msg == "failure") {
+                alert("현재 비빌번호가 틀립니다.\n다시 입력하세요.");
+                userpw.value = "";
+                new_userpasswd.value = "";
+                renew_userpasswd.value = "";
+                location.reload();
+            }
+        }
+    });
+}
+
+function changeName() {
+    if(name.value == "") {
+        alert("변경하실 이름을 입력해 주세요.");
+        name.focus();
+        return false;
+    }
+
+    $.ajax({
+        type: "post",
+        url: "/mypage/changeName",
+        dataType: "json",
+        data:{
+            id: id.value,
+            name: name.value
+        },
+        success: function(result) {
+            if(result.msg == "success") {
+                alert("이름이 변경되었습니다.");
+                location.href = "/mypage/myinfo/update";
+            }else{
+                alert("이름 변경에 문제가 발생하였습니다.\n관리자에게 문의바랍니다.");
+                location.href = "/mypage/myinfo/update";
+            }
+        }
+    });
+}
+
+function changePhone() {
+    if(phone.value == "") {
+        alert("변경하실 전화번호를 입력해 주세요.");
+        phone.focus();
+        return false;
+    }
+
+    $.ajax({
+        type: "post",
+        url: "/mypage/changePhone",
+        dataType: "json",
+        data:{
+            id: id.value,
+            phone: phone.value
+        },
+        success: function(result) {
+            if(result.msg == "success") {
+                alert("휴대폰 번호가 변경되었습니다.");
+                location.href = "/mypage/myinfo/update";
+            }else{
+                alert("휴대폰 번호 변경에 문제가 발생하였습니다.\n관리자에게 문의바랍니다.");
+                location.href = "/mypage/myinfo/update";
+            }
+        }
+    });
+}
+
+function changeEmail() {
+    if(email.value == "") {
+        alert("변경하실 이메일을 입력해 주세요.");
+        email.focus();
+        return false;
+    }
+
+    $.ajax({
+        type: "post",
+        url: "/mypage/changeEmail",
+        dataType: "json",
+        data:{
+            id: id.value,
+            email: email.value
+        },
+        success: function(result) {
+            if(result.msg == "success") {
+                alert("이메일이 변경되었습니다.");
+                location.href = "/mypage/myinfo/update";
+            }else{
+                alert("이메일 변경에 문제가 발생하였습니다.\n관리자에게 문의바랍니다.");
+                location.href = "/mypage/myinfo/update";
+            }
+        }
+    });
+}
+
+function changeAddr() {
+    if(addr.value == "") {
+        alert("변경하실 주소를 입력해 주세요.");
+        addr.focus();
+        return false;
+    }
+
+    $.ajax({
+        type: "post",
+        url: "/mypage/changeAddr",
+        dataType: "json",
+        data:{
+            id: id.value,
+            addr: addr.value
+        },
+        success: function(result) {
+            if(result.msg == "success") {
+                alert("주소가 변경되었습니다.");
+                location.href = "/mypage/myinfo/update";
+            }else{
+                alert("주소 변경에 문제가 발생하였습니다.\n관리자에게 문의바랍니다.");
+                location.href = "/mypage/myinfo/update";
+            }
+        }
+    });
+}
