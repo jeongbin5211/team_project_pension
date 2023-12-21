990aeeaa70cf21c51a72b45c4522aff3394ddd07
jeongbin5211 <hjn123493@gmail.com>
2023-12-21 16:42:42 +0900

    add_qna_answer

--- pension/src/main/resources/static/js/sub/sub_qna/list.js
+++ pension/src/main/resources/static/js/sub/sub_qna/list.js
@@ -1,9 +1,9 @@
-let userid = document.querySelector('input[type=hidden]');
+let userid = document.querySelector('input[name=who]');
 
-    if (userid.value == "") {
-        function sessionRequest() {
-            if (confirm("문의 하시려면 로그인이 필요합니다.\n로그인 하시겠습니까?")) {
-                location.href = "/login";
-            }
+if (userid.value == "") {
+    function sessionRequest() {
+        if (confirm("문의 하시려면 로그인이 필요합니다.\n로그인 하시겠습니까?")) {
+            location.href = "/login";
         }
     }
+}
--- pension/src/main/resources/static/js/sub/sub_qna/view.js
+++ pension/src/main/resources/static/js/sub/sub_qna/view.js
@@ -1,7 +1,3 @@
-function move() {
-  location.href = "/board/notice";
-}
-
 function remove(id) {
   // alert(id);
   if (confirm("해당 문의사항을 삭제하시겠습니까?")) {
@@ -9,3 +5,24 @@ function remove(id) {
     alert("아이디 " + id + "번 문의사항이 삭제되었습니다.");
   }
 }
+
+function modified () {
+    alert("답변이 수정되었습니다.");
+}
+
+let modifyBtn = document.querySelector('.modify_btn');
+let cancelBtn = document.querySelector('.cancel_btn');
+let boardQnaAnswerContent = document.querySelector('.board_qna_answer_content');
+let ansModifyContent = document.querySelector('.ans-modify-content');
+
+modifyBtn.addEventListener('click', () => {
+    boardQnaAnswerContent.classList.add('hide');
+    ansModifyContent.classList.remove('hide');
+});
+
+cancelBtn.addEventListener('click', () => {
+    ansModifyContent.classList.add('hide');
+    boardQnaAnswerContent.classList.remove('hide');
+});
+
+
